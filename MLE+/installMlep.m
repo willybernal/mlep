bb125ec0c9163170921395649dc9298de7d353ee
unknown <wbernalh@wbernalh-w7-vm.nrel.gov>
2015-07-30 16:23:00 -0600

    Remove code to copy RunEPlus.bat file.

--- MLE+/installMlep.m
+++ MLE+/installMlep.m
@@ -18,15 +18,15 @@
 %      open Matlab or pass a path to the savepath function at the end of
 %      the script.
 %
-%      if ispc
-%           % Windows
-%           eplusPath = 'C:\EnergyPlusV8-0-0';
-%           javaPath = 'C:\Program Files\Java\jre7\bin\';
-%      else
-%           % Unix
-%           eplusPath = '/Applications/EnergyPlus-8-0-0';
-%      end
-
+%   if ispc
+%       % Windows
+%       eplusPath = 'C:\EnergyPlusV8-3-0';
+%       javaPath = 'C:\Program Files\Java\jre1.8.0_51\bin';
+%   else
+%       % Unix
+%       eplusPath = '/Applications/EnergyPlus-8-3-0';
+%   end
+% 
 % Last Modified by Willy Bernal - Willy.BernalHeredia@nrel.gov 30-Jul-2015
 
 %% MODIFY
@@ -73,29 +73,29 @@ if ~manualInstall %========================================================
 else %=====================================================================
     % Manual Install
     if ispc
-        if ~exist([eplusPath filesep 'RunEPlus_orig.bat'], 'file') == 2
-            % Rename RunEPlus.bat to RunEPlus_orig.bat
-            [status1,message,messageid] = ...
-                copyfile(fullfile(eplusPath,'RunEPlus.bat'),...
-                fullfile(eplusPath,'RunEPlus_orig.bat'),'f');
-            
-            % Copy RunEPlus.bat
-            [status2,message,messageid] = ...
-                copyfile(fullfile(mlepPath,'settings','RunEPlus.bat'),...
-                fullfile(eplusPath,'RunEPlus.bat'),'f');
-            
-            % Check Whether Replacing RunEPlus was successfull
-            status = status1 && status2;
-            if ~status
-                disp('ERROR: CHECK E+ PATHS AND JAVA PATHS');
-                disp(message);
-                disp(messageid);
-            else
-                disp('MLE+: INSTALLATION COMPLETED!');
-            end
-        else
-            disp('MLE+: INSTALLATION COMPLETED!');
-        end
+%         if ~exist([eplusPath filesep 'RunEPlus_orig.bat'], 'file') == 2
+%             % Rename RunEPlus.bat to RunEPlus_orig.bat
+%             [status1,message,messageid] = ...
+%                 copyfile(fullfile(eplusPath,'RunEPlus.bat'),...
+%                 fullfile(eplusPath,'RunEPlus_orig.bat'),'f');
+%             
+%             % Copy RunEPlus.bat
+%             [status2,message,messageid] = ...
+%                 copyfile(fullfile(mlepPath,'settings','RunEPlus.bat'),...
+%                 fullfile(eplusPath,'RunEPlus.bat'),'f');
+%             
+%             % Check Whether Replacing RunEPlus was successfull
+%             status = status1 && status2;
+%             if ~status
+%                 disp('ERROR: CHECK E+ PATHS AND JAVA PATHS');
+%                 disp(message);
+%                 disp(messageid);
+%             else
+%                 disp('MLE+: INSTALLATION COMPLETED!');
+%             end
+%         else
+%             disp('MLE+: INSTALLATION COMPLETED!');
+%         end
         
     else 
         % Java Path
